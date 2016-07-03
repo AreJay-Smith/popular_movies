12e9a667ec585562339dc9988a95c54ea6b99586
urge-smith <randalljo@gmail.com>
2016-07-03 11:12:41 -0600

    Completely working project

--- app/src/main/java/com/arejaysmith/popularmovies/MovieDetail.java
+++ app/src/main/java/com/arejaysmith/popularmovies/MovieDetail.java
@@ -21,21 +21,8 @@ public class MovieDetail extends AppCompatActivity {
                 .add(R.id.activity_movie_detail, new MovieDetailFragment())
                 .commit();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-//        Movie mMovie = getIntent().getParcelableExtra("test");
-//
-//        TextView mTextTest = (TextView) findViewById(R.id.test);
-//        mTextTest.setText(mMovie.getTitle());
-//
-//        setTitle(mMovie.getTitle());
     }
 }
